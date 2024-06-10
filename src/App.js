904bfd8052f2f74e24561a941bb890fe835c732d
Kitae's Github <aqua0405@gmail.com>
2024-06-11 00:51:37 +0900

    240611-0051

--- src/App.js
+++ src/App.js
@@ -1,9 +1,31 @@
-import './App.css';
+import "./App.css";
+import TodoRowItem from "./components/TodoRowItem";
 
 function App() {
   return (
-    <div>
-      <h1>React App</h1>
+    <div className="mt-5 container">
+      <div className="card">
+        <div className="card-header">Your Todo's</div>
+        <div className="card-body">
+          <table className="table table-hover">
+            <thead>
+              <tr>
+                <th scope="col">#</th>
+                <th scope="col">Description</th>
+                <th scope="col">Assigned</th>
+              </tr>
+            </thead>
+            <tbody>
+              <TodoRowItem />
+              <tr>
+                <th scope="row">2</th>
+                <td>Get haircut</td>
+                <td>Eric</td>
+              </tr>
+            </tbody>
+          </table>
+        </div>
+      </div>
     </div>
   );
 }
---
+++ src/components/TodoRowItem.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+const TodoRowItem = () => {
+  return (
+    <tr>
+      <th scope="row">1</th>
+      <td>Feed dog</td>
+      <td>Eric</td>
+    </tr>
+  );
+};
+
+export default TodoRowItem;
